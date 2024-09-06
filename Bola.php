3292193f9b0b890bf8be700e11a0142f060cf5de
Artazzz <Habibijanuarta@gmail.com>
2024-09-06 15:17:57 +0700

    Praktikum 5A - Memisahkan Class

--- Bola.php
+++ Bola.php
@@ -2,13 +2,7 @@
 class Bola
 {
     const PHI = 3.14;
-    public $jari_jari;
-
-    public function __construct($jari_jari)
-    {
-        $this->jari_jari=$jari_jari;
-    }
-    public function volume() : float {
-        return (4/3)*self::PHI*pow($this->jari_jari,3);
+    public function volume($jari_jari) : float {
+        return (4/3)*self::PHI*pow($jari_jari,3);
     }
 }
--- Tabung.php
+++ Tabung.php
@@ -1,8 +1,8 @@
 <?php
-class Kerucut
+class Tabung
 {
     const PHI = 3.14;
     public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+        return self::PHI*pow($jari_jari,2)*$tinggi;
     }
 }
