c5aeceba95feb82b3d86ed2a3681362f0bb7a1ed
Artazzz <Habibijanuarta@gmail.com>
2024-09-19 20:49:36 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Admin;
+
+require_once 'Pegawai.php';
+
+class TenagaKependidikan extends Pegawai {
+    public $gaji_pokok;
+
+    public function __construct($nip, $nama, $no_hp, $alamat, $gaji_pokok) {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->gaji_pokok = $gaji_pokok;
+    }
+
+    public function cuti() {
+    }
+}
